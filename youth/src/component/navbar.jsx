a6f529070381d1aa7dba23e5b6af51837a438e7b
kirtisule <kirtisule27111996@gmail.com>
2023-05-06 00:41:00 +0530

    done

---
+++ youth/src/component/navbar.jsx
@@ -0,0 +1,103 @@
+
+function Navbar() {
+    return (
+        <>
+        <div>
+      
+     <div style={{border:"2px solid red",width:"100%",height:"50px",display:"flex"}}>
+        
+        <div >
+        <a href="">For You</a>
+        <a href="">Discover</a>
+        <a href="">live</a>
+        <a href="">Hire</a>
+        <a href="">Jobs</a>
+        </div>
+        <div>
+        <input placeholder="Search"/>
+        <button>Search</button>
+        </div>
+        <div>
+            <button>Log In</button>
+            <button>Sign Up</button>
+        </div>
+     </div>
+     </div>
+     
+
+      <div>
+        <h4>HEALTH DEVICES CARNIVAL</h4>
+      </div>
+     <div style={{border:"2px solid red",display:"flex", gap:"10px",justifyContent:"center"}}>
+        <div style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/c/o/con0443-1-jan16.jpg" alt="" />
+            <h4>Rs. 854</h4>
+            <button>ADD TO CART</button>
+        </div>
+        <div  style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/o/n/one0172_1.jpg" alt="" />
+            <h4>Rs. 546</h4>
+            <button>ADD TO CART</button>
+        </div>
+        <div  style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/f/r/fre0348-1_1.jpg" alt="" />
+            <h4>Rs. 645</h4>
+            <button>ADD TO CART</button>
+        </div>
+        <div  style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/a/c/acc0008_1.jpg" alt="" />
+            <h4>Rs. 753</h4>
+            <button>ADD TO CART</button>
+        </div>
+        <div  style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/7/1/7156ap_2.png" alt="" />
+            <h4>Rs. 553</h4>
+            <button>ADD TO CART</button>
+        </div>
+        <div  style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/c/o/covidfind_box_1.jpg" alt="" />
+            <h4>Rs. 773</h4>
+            <button>ADD TO CART</button>
+        </div>
+     </div>
+     <div>
+        <h4>WEIGHT MANAGEMENT ESSENTIALS</h4>
+      </div>
+      <div style={{border:"2px solid red",display:"flex", gap:"10px",justifyContent:"center"}}>
+        <div style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/g/o/gof0016-1-jan23_2_.jpg" alt="" />
+            <h4>Rs. 554</h4>
+            <button>ADD TO CART</button>
+        </div>
+        <div  style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/w/b/wbn0017_2_.jpg" alt="" />
+            <h4>Rs. 746</h4>
+            <button>ADD TO CART</button>
+        </div>
+        <div  style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/g/o/gof0018-1-jan23_2_.jpg" alt="" />
+            <h4>Rs. 735</h4>
+            <button>ADD TO CART</button>
+        </div>
+        <div  style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/e/n/end0182_1-feb23.jpg" alt="" />
+            <h4>Rs. 543</h4>
+            <button>ADD TO CART</button>
+        </div>
+        <div  style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/g/o/gof0017-1-jan23_2_.jpg" alt="" />
+            <h4>Rs. 345</h4>
+            <button>ADD TO CART</button>
+        </div>
+        <div  style={{border:"2px solid green"}}>
+            <img style={{width:"200px",height:"150px"}} src="https://newassets.apollo247.com/pub/media/catalog/product/a/p/apf0022_1-march15_6_.jpg" alt="" />
+            <h4>Rs. 876</h4>
+            <button>ADD TO CART</button>
+        </div>
+     </div>
+  </>
+
+);
+}
+
+export default Navbar;
